46562ba78b3c77e8c26654b31ddf6db94709687b
John Siracusa <siracusa@gmail.com>
2025-01-04 15:13:06 -0500

    Initial Commit

---
+++ SubscriptionStoreViewTabView/SubscriptionStoreViewTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionStoreViewTabViewApp.swift
+//  SubscriptionStoreViewTabView
+//
+//  Created by John Siracusa on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionStoreViewTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
